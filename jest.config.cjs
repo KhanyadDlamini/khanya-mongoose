4ad9ddebedadfedb3367490f9d183908a4d08401
KhanyadDlamini <khanyad.dlamini@gmail.com>
2025-11-05 14:44:11 +0200

    Fixing the workflows

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
-    // Use Node.js environment since we are testing backend code
     testEnvironment: "node",
-
-    // Match test files
-    testMatch: ["**/__tests__/**/*.test.js"],
-
-    // Enable ESM support automatically (because "type": "module" is in package.json)
+    extensionsToTreatAsEsm: [".js"],
+    testMatch: ["**/test.js"],  // <-- include the file path
     transform: {},
-
-    // Verbose test output
-    verbose: true,
-
-    // Run tests in serial (optional, sometimes better for DB tests)
-    maxWorkers: 1,
 };
